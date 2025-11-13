cc86dbbfa2b3ee8553bd03fb39871281bc017435
saini-manisha <manishasaini46160@gmail.com>
2025-11-13 21:54:43 +0530

    learn stream

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,34 @@
+import java.util.List;
+
 public class Main {
-    char var;// default value will assign in class data variable only not in the local variable
+    char var;
+    boolean boolvar;// default value will assign in class data variable only not in the local variable
     public static void main(String[] args) {
-        Main m=new Main();
-        System.out.println(m.var);
-//        int x;
-//        System.out.println(x);// this line is giving error because x is local varialbe and it is not initialized with default value, default value will be assigned to the data member varialble only
+//        Main m=new Main();
+//        //System.out.println(m.var);
+//        long x=23l;
+//        int integVar=130;
+//        byte byteInt=(byte) integVar;
+//       // System.out.println(byteInt);
+//        System.out.println(new Main().boolvar);
+////        int x;
+////        System.out.println(x);// this line is giving error because x is local varialbe and it is not initialized with default value, default value will be assigned to the data member varialble only
+
+
+
+
+//        double a = 0.1;
+//        double b = 0.2;
+//        double c = a + b;
+//
+////        float a=.7f;
+//        System.out.println(c);
+//        String s1="hello";
+//        String s2="hello";
+//        String s3=new String("hello");
+//        System.out.println(s1.equals(s2));
+        List<Integer> numbers= List.of(12,23,45,66);
+        List<Integer> numberAddWith5=numbers.stream().map((item)->item+5).toList();
+        System.out.println(numberAddWith5);
     }
 }
